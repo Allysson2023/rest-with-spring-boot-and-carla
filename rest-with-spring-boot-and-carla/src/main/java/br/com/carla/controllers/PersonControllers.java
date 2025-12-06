fc222f2c317c29b19aff91decd515bf73c2b1985
Allysson <allyssoncarlos.ac21@gmail.com>
2025-12-06 13:12:09 -0300

    Adicionando o PersonServices, Controller e Person somente no Mock

---
+++ rest-with-spring-boot-and-carla/src/main/java/br/com/carla/controllers/PersonControllers.java
@@ -0,0 +1,53 @@
+package br.com.carla.controllers;
+
+import br.com.carla.PersonServices;
+import br.com.carla.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonControllers {
+
+    @Autowired
+    private PersonServices services;
+
+    @RequestMapping(value = "{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") String id){
+        return services.findById(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findByAll(){
+        return services.findByAll();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person){
+        return services.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person){
+        return services.update(person);
+    }
+
+    @RequestMapping(value = "{id}",
+            method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id){
+        services.delete(id);
+    }
+}
