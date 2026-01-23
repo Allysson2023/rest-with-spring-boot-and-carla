3dae7574bb5cefd0a535b40640118828ba9f7c27
Allysson <allyssoncarlos.ac21@gmail.com>
2026-01-23 15:43:49 -0300

    Testando a Geração dos Links HATEOAS no Servico com Mockito e jUnit 5 parte 1

---
+++ rest-with-spring-boot-and-carla/src/test/java/br/com/carla/services/PersonServicesTest.java
@@ -0,0 +1,72 @@
+package br.com.carla.services;
+
+import br.com.carla.model.Person;
+import br.com.carla.repository.PersonRepository;
+import br.com.carla.unitetests.mapper.mocks.MockPerson;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServicesTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private  PersonServices services;
+
+    @Mock
+    PersonRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = services.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+
+        assertNotNull(result.getLinks().stream().anyMatch(link -> link.getRel().value().equals("self")
+                        && link.getHref().endsWith("/person/v1/1")
+                        && link.getType().equals("GET"))
+        );
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findByAll() {
+    }
+}
